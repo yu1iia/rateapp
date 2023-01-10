db8a4bfffcbb1a7b340d6c8266b1ed89ccdc5927
Yuliia Burko <yuliia.burko@gmail.com>
2023-01-10 21:33:12 +0100

    added initial files

--- README.md
+++
@@ -1,2 +0,0 @@
-# rateapp
-react practice
---
+++ app/src/components/layout/Layout.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './layout.scss';
+import { AddClass } from '../../hoc/AddClass';
+
+const Layout = () => {
+  return <h1>Layout</h1>;
+};
+
+export default AddClass(Layout, 'layout');
