f35759dab409596948a5c12a7028f395db41b1c7
Yuliia Burko <yuliia.burko@gmail.com>
2023-01-10 23:23:16 +0100

    added home/sidebar sections

--- app/src/components/layout/Layout.js
+++ app/src/components/layout/Layout.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './layout.scss';
 import { AddClass } from '../../hoc/AddClass';
 import { Header } from '../header/Header';
+import { Home } from '../../pages/home/Home';
+import { Sidebar } from '../../components/sidebar/Sidebar';
 
 const Layout = () => {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <div className="content">
+        <div className="routes">
+          <Home />
+        </div>
+        <Sidebar />
+      </div>
+    </Fragment>
+  );
 };
 
 export default AddClass(Layout, 'layout');
---
+++ app/src/components/sidebar/Sidebar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './sideBar.scss';
+
+export const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarHead">
+        <h3>Все валюты</h3>
+      </div>
+      <div className="sidebarContent">
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
