4997af2dce74131864d505389691f3a11e7c0b85
Yuliia Burko <yuliia.burko@gmail.com>
2023-01-17 22:28:06 +0100

    added sample page

--- app/src/components/button/Button.js
+++ app/src/components/button/Button.js
@@ -5,7 +5,7 @@ export const Button = props => {
   return (
     <button
       className="btn"
-      onClick={() => props.click(props.arg || '') || undefined}
+      onClick={() => (props.click ? props.click(props.arg || '') : undefined)}
     >
       {props.text}
     </button>
--- app/src/pages/sample/Sample.js
+++ app/src/pages/sample/Sample.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { RateContext } from '../../context/RateContext';
+import { Button } from '../../components/button/Button';
 
 import './sample.scss';
 
 export const Sample = () => {
-  return <h1>Sample</h1>;
+  const { state } = useContext(RateContext);
+
+  return (
+    <div className="sample">
+      <div className="sampleContainer">
+        <div>
+          <h3>
+            Получить курс: &nbsp;
+            <select>
+              {Object.keys(state.currency).map((item, index) => {
+                return <option key={item}>{item}</option>;
+              })}
+            </select>
+            &nbsp; &nbsp; к &nbsp;&nbsp;
+            <select>
+              {Object.keys(state.currency).map((item, index) => {
+                return <option key={item}>{item}</option>;
+              })}
+            </select>
+          </h3>
+        </div>
+        <div className="sampleHead">
+          <span>
+            Дата
+            <input type="date" />
+          </span>
+          <Button text="Получить курс" />
+        </div>
+        <div className="sampleResult">
+          <ul></ul>
+        </div>
+      </div>
+    </div>
+  );
 };
